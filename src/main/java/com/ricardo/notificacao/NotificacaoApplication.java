376673f43b8a31155d131d902b06064ce55ae070
Ricardo Capeletti <ricardolopescap@gmail.com>
2026-01-28 19:12:47 -0300

    commit inicial

--- src/main/java/com/ricardo/notificacao/NotificacaoApplication.java
+++ src/main/java/com/ricardo/notificacao/NotificacaoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotificacaoApplication {
 
+	//descricao//
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacaoApplication.class, args);
 	}
